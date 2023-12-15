08962938466846aba82295797d1dbfc5a092cbf4
yyz20002008 <yyz20002008@gmail.com>
2023-12-14 21:24:25 -0500

    use scheduler

--- app.py
+++ app.py
@@ -244,8 +244,10 @@ def search():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all() # <--- create db object.
-    thread1 = threading.Thread(target=NA1,name='NAThread')
-    thread1.start()
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(NA1, 'interval', hours=1)
+    #thread1 = threading.Thread(target=NA1,name='NAThread')
+    #thread1.start()
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
 
